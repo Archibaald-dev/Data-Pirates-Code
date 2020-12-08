4d5c5f0ad6a24fdef4d9255e37db23b6f3fbe533
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-12-08 15:11:35 +0100

    Create articles feature

---
+++ come/src/features/articles/components/articlesElements/ArticlesElements.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+
+
+
+export default class ArticlesElements extends Component{
+    render() {
+        return(
+            <div className="container">
+                <div className="image"></div>
+                <div className="text"></div>
+
+            </div>
+        )
+    }
+}
