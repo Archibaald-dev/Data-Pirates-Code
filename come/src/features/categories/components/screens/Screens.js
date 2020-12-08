f43b47a10dcd1de9a4e0f5a54cf62a7b188acdf0
Archibaald-dev <milo.lallement@yahoo.fr>
2020-12-08 19:08:23 +0100

    Milo part done

--- come/src/features/categories/components/screens/Screens.js
+++ come/src/features/categories/components/screens/Screens.js
@@ -1,105 +1,116 @@
 import React, { Component } from 'react'
+import one from '../../../../Assets/screen/1.png'
+import two from '../../../../Assets/screen/2.png'
+import three from '../../../../Assets/screen/3.png'
+import four from '../../../../Assets/screen/4.png'
+import five from '../../../../Assets/screen/5.png'
+import six from '../../../../Assets/screen/6.png'
+import seven from '../../../../Assets/screen/7.png'
+import eight from '../../../../Assets/screen/8.png'
+
 
 
 
 export default class Screens extends Component {
     render() {
         return (
+            <div>
             <div className="container">
                 <div className="row">
                     <div className="col-sm">
                         <div className="card" >
-                            <img className="card-img-top" src="../../../../Assets/screen/2.png" alt="Card image cap"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Asus</h5>
-                                    <p className="card-text">This is a super screen.</p>
-                                    
-                                </div>
-</div>
-                        </div>
+                            <img className="card-img-top" src={one} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">Asus</h5>
+                                <p className="card-text">This is a super screen.</p>
 
-                        <div className="col-sm">
-                            <div className="card" >
-                                <img className="card-img-top" src="../../../../Assets/screen/3.png" alt="Card image cap"/>
-                                    <div className="card-body">
-                                        <h5 className="card-title">Lenovo</h5>
-                                        <p className="card-text">This is a super cool screen.</p>
-                                        
-                                    </div>
-</div>
                             </div>
+                        </div>
+                    </div>
 
-                            <div className="col-sm">
-                                <div className="card" >
-                                    <img className="card-img-top" src="../../../../Assets/screen/4.png" alt="Card image cap"/>
-                                        <div className="card-body">
-                                            <h5 className="card-title">Apple</h5>
-                                            <p className="card-text">This is a super strong screen.</p>
-                                            
-                                        </div>
-</div>
-                                </div>
-
-                                <div className="col-sm">
-                                    <div className="card" >
-                                        <img className="card-img-top" src="../../../../Assets/screen/2.png" alt="Card image cap"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Asus</h5>
-                                                <p className="card-text">This is a screen.</p>
-                                                
-                                            </div>
-</div>
-                                    </div>
-
-                                </div>
-
-                                <div className="row">
                     <div className="col-sm">
                         <div className="card" >
-                            <img className="card-img-top" src="../../../../Assets/screen/5.png" alt="Card image cap"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Asus</h5>
-                                    <p className="card-text">This is a super screen.</p>
-                                    
-                                </div>
-</div>
-                        </div>
+                            <img className="card-img-top" src={three} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">Lenovo</h5>
+                                <p className="card-text">This is a super cool screen.</p>
 
-                        <div className="col-sm">
-                            <div className="card" >
-                                <img className="card-img-top" src="../../../../Assets/screen/6.png" alt="Card image cap"/>
-                                    <div className="card-body">
-                                        <h5 className="card-title">Lenovo</h5>
-                                        <p className="card-text">This is a super cool screen.</p>
-                                        
-                                    </div>
-</div>
                             </div>
+                        </div>
+                    </div>
 
-                            <div className="col-sm">
-                                <div className="card" >
-                                    <img className="card-img-top" src="../../../../Assets/screen/7.png" alt="Card image cap"/>
-                                        <div className="card-body">
-                                            <h5 className="card-title">Apple</h5>
-                                            <p className="card-text">This is a super strong screen.</p>
-                                            
-                                        </div>
-</div>
-                                </div>
+                    <div className="col-sm">
+                        <div className="card" >
+                            <img className="card-img-top" src={four} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">Apple</h5>
+                                <p className="card-text">This is a super strong screen.</p>
 
-                                <div className="col-sm">
-                                    <div className="card" >
-                                        <img className="card-img-top" src="../../../../Assets/screen/8.png" alt="Card image cap"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Asus</h5>
-                                                <p className="card-text">This is a screen.</p>
-                                                
-                                            </div>
-</div>
-                                    </div>
+                            </div>
+                        </div>
+                    </div>
 
-                                </div>
-                                </div>
+                    <div className="col-sm">
+                        <div className="card" >
+                            <img className="card-img-top" src={six} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">Asus</h5>
+                                <p className="card-text">This is a screen.</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="row">
+                    <div className="col-sm">
+                        <div className="card" >
+                            <img className="card-img-top" src={five} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">Asus</h5>
+                                <p className="card-text">This is a super screen.</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-sm">
+                        <div className="card" >
+                            <img className="card-img-top" src={two} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">Lenovo</h5>
+                                <p className="card-text">This is a super cool screen.</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-sm">
+                        <div className="card" >
+                            <img className="card-img-top" src={seven} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">Apple</h5>
+                                <p className="card-text">This is a super strong screen.</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-sm">
+                        <div className="card" >
+                            <img className="card-img-top" src={eight} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">Asus</h5>
+                                <p className="card-text">This is a screen.</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            </div>
         )
     }
 }
