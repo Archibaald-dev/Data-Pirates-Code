1a63dd6a137feb8d54156493e59429fde98a6edd
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-12-08 15:11:12 +0100

    Create squaredMenu feature

---
+++ come/src/features/squaredMenu/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Squared} from "./components";
+
+
+const SquaredMenu = () =>{
+        return(
+            <div>
+                <Squared/>
+            </div>
+        )
+}
+
+
+export default SquaredMenu
