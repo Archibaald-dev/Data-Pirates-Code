d10d0582bd18fce2a354a24ffb944ab9d32a41d0
Gautier78 <Gautier-lemaitre@hotmail.com>
2020-12-08 15:09:26 +0000

    HomePage + img

---
+++ come/src/features/homePage/components/index.js
@@ -0,0 +1,1 @@
+export {default as Squared} from './home/Home'
---
+++ come/src/features/homePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Home} from "./components";
+
+
+const HomePage = () =>{
+        return(
+            <div>
+                <Home/>
+            </div>
+        )
+}
+
+
+export default HomePage
